6457a6917cb82278feefce2379dfd2d74a92c6ed
Prince-willy <140852401+Prince-willy@users.noreply.github.com>
2024-12-24 10:58:00 +0100

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ function convertToBool(text, fault = 'true') {
 
 //--------------------------Mr Frank---------------------------------------
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SUBZEFOSESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "WIgR3I7J#iv4fPCO045Bw2R1kD10OghSOB1YAeiAVPkj3pxVrPEc",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //TRUE OR FALSE
 MODE: process.env.MODE || "public",  // PRIVATE OR PUBLIC
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
